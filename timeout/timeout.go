dc00e63092177c38d0f32e21d0117d745dec6074
progfay <shun1856shun@gmail.com>
2020-08-28 23:49:53 +0900

    doc: add godoc

--- timeout/timeout.go
+++ timeout/timeout.go
@@ -5,10 +5,14 @@ import (
 	"time"
 )
 
+// SetTimeout sets a timer which executes a function once the timer expires.
+// To abort timer, call returned function.
 func SetTimeout(callback func(), delay time.Duration) (cancel func()) {
 	return SetTimeoutWithContext(context.Background(), callback, delay)
 }
 
+// SetTimeoutWithContext sets a timer which executes a function once the timer expires.
+// When ctx is cancelled, timer is aborted.
 func SetTimeoutWithContext(ctx context.Context, callback func(), delay time.Duration) func() {
 	childCtx, cancel := context.WithCancel(ctx)
 	timer := time.NewTimer(delay)
